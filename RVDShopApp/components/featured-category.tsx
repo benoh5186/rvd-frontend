ba54f959408d5538d78e4459be4f9477f8126474
Ben Oh <moh16@my.bcit.ca>
2026-02-13 17:41:04 -0800

    add FetauredCategory component with 4 states that renders products of featured category

---
+++ RVDShopApp/components/featured-category.tsx
@@ -0,0 +1,10 @@
+import { useEffect, useState } from 'react';
+
+export default function FeaturedCategory() {
+    const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [fail, setFailure] = useState(null);
+    const [refresh, setRefresh] = useState(false);
+
+    
+}
