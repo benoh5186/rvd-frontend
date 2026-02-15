5daa2d01cf5ee86b7f59f5427a82a308da115668
Ben Oh <moh16@my.bcit.ca>
2026-02-14 17:55:54 -0800

    finish implementation of FeaturedCategory component

--- RVDShopApp/components/featured-category.tsx
+++ RVDShopApp/components/featured-category.tsx
@@ -1,4 +1,6 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useCallback } from 'react';
+import { ActivityIndicator } from 'react-native';
+import { FlatList, View, Text} from 'react-native-reanimated/lib/typescript/Animated';
 
 export default function FeaturedCategory() {
     const [products, setProducts] = useState<any[]>([]);
@@ -11,9 +13,11 @@ export default function FeaturedCategory() {
             if (refresh) {
                 setRefresh(true)
             } else {
-                setLoading(true)
-                // fetch logic        
+                setLoading(true)  
             }
+            const response = await fetch("https://fakestoreapi.com/products")
+            const data = await response.json()
+            setProducts(data)
         }
         catch {
             setFailure("Network error")
@@ -22,12 +26,33 @@ export default function FeaturedCategory() {
             setLoading(false)
             setRefresh(false)
         }
-
-
     }
 
     useEffect(() => {
         loadProducts(false)
     }, [])
+
+    const onRefresh = useCallback(() => {
+        loadProducts(true);
+    }, [loadProducts])
+
+    if (loading) {
+        return <ActivityIndicator size="large"/>;
+    }
+    if (fail) {
+        return <Text>{"Network Error"}</Text>
+    }
+    return (
+        <View style={{ flex: 1 }}>
+            <FlatList 
+            data={products}
+            keyExtractor={(item) => item.id}
+            renderItem={({item}) => <Text>{item.title}</Text>}
+            refreshing={refresh}
+            onRefresh={onRefresh}  
+            />
+        </View>
+    )
+
 
 }
